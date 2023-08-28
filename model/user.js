df8a10739d0751943cf803b33730d7b39e70ca25
Yahckman <adeniranajibade2014@gmail.com>
2023-08-28 10:35:14 +0100

    adding jwt Private key

--- model/user.js
+++ model/user.js
@@ -13,7 +13,7 @@ const userSchema = new mongoose.Schema({
 userSchema.methods.generateAuthToken = function () {
   const token = jwt.sign(
     { _id: this._id, name: this.name, email: this.email, phone: this.phone },
-    "12345"
+    process.env.JWT_PRIVATE_KEY
   );
 
   return token;
